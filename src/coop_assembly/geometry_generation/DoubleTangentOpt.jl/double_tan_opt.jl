8afa6b421964b5960a16936b7b365b8dfbe8b601
Yijiang Huang <yijiangh@mit.edu>
2020-11-04 19:22:37 -0500

    [WIP] viz contact lines in jl

--- src/coop_assembly/geometry_generation/DoubleTangentOpt.jl/double_tan_opt.jl
+++ src/coop_assembly/geometry_generation/DoubleTangentOpt.jl/double_tan_opt.jl
@@ -31,7 +31,12 @@ const DATA_DIR = joinpath(ROOT_DIR, "tests", "test_data")
 const SCALE = 1e-2
 const COORDINATES = ["x", "y", "z"]
 
+# edge id format
 const EDGE_ID = Set
+
+const LENGTH_TOLERANCE = SCALE * 10.0
+const BUFFER_TOLERANCE = SCALE * 0.0
+const CONTACT_TOLERANCE = SCALE * 0.0
 
 #########################################
 
@@ -51,34 +56,46 @@ end
 """
 - edge: EDGE_ID((node_id1, node_id2))
 - nodes: Dict( node_id => node info)
-- egdes: Dict( edge_id => edge info)
+- edges: Dict( edge_id => edge info)
+- fraction: the fraction of the full bar length to consider
+- spacing: spacing ratio, step_size = bar radius if `spacing = 1`
 """
-function enumerate_steps(edge, nodes, edge_from_id, fraction=0.1, spacing=1.5) # 1e-3 | 0.1
+function enumerate_steps(edge, nodes, edge_from_id, fraction=0.1, spacing=1.0) # 1e-3 | 0.1 | 1.5
     step_size = spacing * edge_from_id[edge]["radius"]
     num_steps = ceil(Int, fraction * get_length(edge, nodes) / step_size)
     return range(0., stop=fraction, length=num_steps)
 end
 
 function convex_combination(x1, x2, w=0.5)
-    return w * x1 + (1-w) * x2
+    return w * x2 + (1-w) * x1
 end
 
+"""
+Compute closest line between two line segments, using an unconstrained quadratic optimization
+"""
 function closest_point_segments(line1, line2)
     m = Model(optimizer_with_attributes(Ipopt.Optimizer, "print_level" => 0))
+    # m = Model(optimizer_with_attributes(Gurobi.Optimizer, "OutputFlag" => false))
     @variable(m, 0 <= γ1 <= 1)
     @variable(m, 0 <= γ2 <= 1)
     @objective(m, Min, sum((convex_combination(line1..., γ1) - convex_combination(line2..., γ2)).^2))
 
     optimize!(m)
-    @show distance = objective_value(m)
-    @show g1 = value.(γ1)
-    @show g2 = value.(γ2)
+    # distance = objective_value(m)
+    g1 = value.(γ1)
+    g2 = value.(γ2)
     return convex_combination(line1..., g1), convex_combination(line2..., g2)
 end
 
 ##################################################
 
-function create_hint(nodes, edges, shrink=true)
+"""
+create an initial guess using the given original graph
+
+Return:
+- Dict: (edge, node) => point coordinate
+"""
+function create_x_hint(nodes, edges, shrink=true)
     hint_solution = Dict()
     for edge in keys(edges)
         for node in edge
@@ -130,32 +147,34 @@ function center_viewer(nodes, pitch=-π/8, distance=2)
     return PyPb.draw_pose(PyPb.Pose(point=centroid), length=1)
 end
 
-function visualize_solution(edges, x_sol, z_sol, alpha=0.25)
+"""
+validate and visualize solutions
+"""
+function visualize_solution(nodes, edges, x_sol, z_sol, alpha=0.25)
+    render_lock = PyPb.LockRenderer()
     bodies = []
     edge_points = DefaultDict(Vector{Vector})
     for ((edge, node), point) in x_sol
         push!(edge_points[edge], point)
-    #     draw_point(point, size=2*edges[edge]["radius"], color=BLUE)
-    #     body = create_sphere(edges[edge]["radius"], color=apply_alpha(BLUE, 0.25), mass=STATIC_MASS)
-    #     set_point(body, point)
+        PyPb.draw_point(point, size=2*edges[edge]["radius"], color=PyPb.BLUE)
+        body = PyPb.create_sphere(edges[edge]["radius"], color=PyPb.apply_alpha(PyPb.BLUE, 0.25), mass=PyPb.STATIC_MASS)
+        PyPb.set_point(body, point)
 
-        # point2 = solution[edge, get_other(edge, node)]
-        # for l in enumerate_steps(edge, nodes, edges)
-        #     trailing = (1 - l) * point + (l * point2)
-        #     body = create_sphere(edges[edge]["radius"], color=apply_alpha(BLUE, 0.25), mass=STATIC_MASS)
-        #     set_point(body, trailing)
-        # end
+        point2 = x_sol[edge, get_other(edge, node)]
+        for l in enumerate_steps(edge, nodes, edges)
+            trailing = convex_combination(point, point2, l)
+            body = PyPb.create_sphere(edges[edge]["radius"], color=PyPb.apply_alpha(PyPb.BLUE, 0.25), mass=PyPb.STATIC_MASS)
+            PyPb.set_point(body, trailing)
+        end
     end
 
     # TODO: analyze collisions and proximity
     for (edge, points) in edge_points
         point1, point2 = points
-        point1 = points[1]
-        point2 = points[2]
-        println("E#$edge: $point1 | $point2 | L: $(norm(point1-point2))")
+        # println("E#$edge: $point1 | $point2 | L: $(norm(point1-point2))")
         element = create_element(point1, point2, edges[edge]["radius"]; color=PyPb.apply_alpha(PyPb.RED, 0.25))
         push!(bodies, create_element(point1, point2, edges[edge]["radius"], color=PyPb.apply_alpha(PyPb.RED, alpha)))
-        # PbPb.add_line(point1, point2, color=BLUE)
+        PyPb.add_line(point1, point2, color=PyPb.BLUE)
     end
 
     # * draw contact lines
@@ -163,40 +182,60 @@ function visualize_solution(edges, x_sol, z_sol, alpha=0.25)
         contact_pt1, contact_pt2 = closest_point_segments(edge_points[edge1], edge_points[edge2])
         contact_distance = norm(contact_pt1-contact_pt2)
         radius_distance = sum(edges[edge]["radius"] for edge in [edge1, edge2])
-        if z_var != 1
-            @assert contact_distance > radius_distance "contact_distance $(contact_distance) should be bigger than radius_distance $(radius_distance)!"
+
+        var1, var2 = x_sol[edge1, node], x_sol[edge2, node]
+        approx_distance = norm(var2 - var1)
+
+        println(BLUE_FG("(($edge1, $edge2), $node) : $z_var"))
+        println(BLUE_FG("Approx distance $(approx_distance) | Accurate $(contact_distance)"))
+        if z_var ≈ 0.0
+            # ! collision constraint
+            # collision_con = @constraint(model, sum(difference .* difference) >= (radius_distance + buffer_tolerance)^2)
+            # @assert
+            @warn contact_distance ≥ (radius_distance + BUFFER_TOLERANCE) "contact_distance $(contact_distance) should be bigger than radius_distance $(radius_distance)!"
             continue
         else
-            point1 = value.(point1_var)
-            point2 = value.(point2_var)
-            # 2. * edges[edge]['radius']
-            PyPb.draw_point(point1, color=PyPb.GREEN)
-            PyPb.draw_point(point2, color=PyPb.GREEN)
-            PyPb.add_line(point1, point2, color=PyPb.GREEN)
+            # ! contact constraint
+            # contact_con = @constraint(model, sum(difference .* difference) <= (radius_distance + contact_tolerance)^2 + (1 - z_var) * max_distance^2)
+            @warn (radius_distance + BUFFER_TOLERANCE) ≤ contact_distance ≤ (radius_distance + CONTACT_TOLERANCE) "contact_distance $(contact_distance) should be with range [$(radius_distance + BUFFER_TOLERANCE), $(radius_distance + CONTACT_TOLERANCE)]!"
+
+            PyPb.draw_point(contact_pt1, color=PyPb.GREEN)
+            PyPb.draw_point(contact_pt2, color=PyPb.GREEN)
+            PyPb.add_line(contact_pt1, contact_pt2, color=PyPb.GREEN)
             # print(str_from_object(pair), value.(l1_var), value.(l2_var),
             #       radius_distance, norm(point1-point2))
         end
     end
-
+    render_lock.restore()
 end
 
 #########################################
 
 """
 - length_tolerance: tolerance for bar's adherence to the original graph edge length
+- contact_tolerance : eps added to the contact constraint
+- buffer_tolerance : eps added to the collision constraint
 """
 function solve(nodes, edges, aabb;
-               hint_solution=nothing,
                optimize=true,
+               check_feasible=false,
+               hint_x_solution=nothing,
+               hint_z_solution=nothing,
                min_tangents=2, # 2 | INF
-               length_tolerance=SCALE*10, contact_tolerance=SCALE*1, buffer_tolerance=SCALE*0,
+               length_tolerance=LENGTH_TOLERANCE, contact_tolerance=CONTACT_TOLERANCE, buffer_tolerance=BUFFER_TOLERANCE,
                num_solutions=1, max_time=1*60, verbose=true)
     println("-"^20)
     max_distance = norm(aabb[2] - aabb[1])
 
-    if hint_solution === nothing
+    @assert !check_feasible || (hint_x_solution!==nothing && hint_z_solution!==nothing)
+    if check_feasible
+        optimize = false
+        println(YELLOW_FG("Checking feasiblity."))
+    end
+
+    if hint_x_solution === nothing
         # TODO: diagnose initial infeasibility
-        hint_solution = create_hint(nodes, edges)
+        hint_x_solution = create_x_hint(nodes, edges)
     end
 
     # * Define the model
@@ -215,11 +254,10 @@ function solve(nodes, edges, aabb;
     objective = Dict()
     for edge in keys(edges)
         for node in edge
-            # var = np_var(model, lower=aabb.lower, upper=aabb.upper)
             vars = @variable(model, [i=1:3],
                 lower_bound=aabb[1][i], upper_bound=aabb[2][i],
-                base_name="x[($(EDGE_ID(edge)),$node]",
-                start=hint_solution[edge, node][i])
+                base_name="x[($(edge),$node]",
+                start=hint_x_solution[edge, node][i])
             difference = vars .- nodes[node]["point"]
             x_vars[edge, node] = vars
             objective[edge, node] = sum(difference.*difference)
@@ -242,31 +280,36 @@ function solve(nodes, edges, aabb;
 
     if optimize
         @objective(model, Min, sum(values(objective)))
+    else
+        # https://github.com/jump-dev/JuMP.jl/issues/693#issuecomment-272638604
+        @objective(model, Min, 0)
     end
 
+    # collect connectivity info
     adjacent_edge_from_node = DefaultDict(Set)
     for edge in keys(edges)
         for node in edge
             push!(adjacent_edge_from_node[node], edge)
         end
     end
+    # @show adjacent_edge_from_node
 
     z_vars = Dict()
     for (node, neighbors) in adjacent_edge_from_node
         # num_tangents = min(len(neighbors) - 1, 2)
         # print(len(edges), num_tangents)
         for pair in map(Set, subsets(collect(neighbors), 2))
-            var = @variable(model, binary=true)
-            set_name(var, "z[$(pair), $(node)]")
-            z_vars[pair, node] = var
+            z_vars[pair, node] = @variable(model, binary=true, base_name="z[$(pair), $(node)]")
         end
     end
-    @show adjacent_edge_from_node
 
-    z_var_from_edge = DefaultDict(Dict())
+    # z_var_from_edge = DefaultDict(Vector)
+    z_var_from_edge = DefaultDict(Dict)
     for ((pair, node), var) in z_vars
+        # for edge in pair
         for (i, edge) in enumerate(pair)
             z_var_from_edge[edge, node][i] = var
+            # push!(z_var_from_edge[edge, node], var)
         end
     end
 
@@ -275,16 +318,16 @@ function solve(nodes, edges, aabb;
     for ((edge, node), neighbors) in z_var_from_edge
         num_tangents = min(length(neighbors), min_tangents)
         # degree = len(neighbors) + 1
-        @printf("Neighbors: %s | Tangents: %s\n", length(neighbors), num_tangents)
-        # model.addConstr(sum(neighbors) == num_tangents)
-        ne_con = @constraint(model, sum(values(neighbors)) == num_tangents)
-        set_name(ne_con, "NeCon[$(EDGE_ID(edge)), $node]")
+        println("E#$(edge)-N$(node) : Neighbors: $(length(neighbors)) | Tangents: $(num_tangents)")
 
+        ne_con = @constraint(model, sum(values(neighbors)) == num_tangents)
+        set_name(ne_con, "NeCon[$(edge), $node]")
+
+        # if only has two neighboring bars, must touch both of them
         if length(neighbors) == num_tangents
             for z_var in values(neighbors)
                 fix(z_var, 1; force = true)
-                # set_lower_bound(z_var, 1)
-                # set_upper_bound(z_var, 1)
+                @assert is_fixed(z_var)
             end
         end
     end
@@ -307,7 +350,7 @@ function solve(nodes, edges, aabb;
             z_var = z_vars[pair, node1]
             # ! contact constraint
             contact_con = @constraint(model, sum(difference .* difference) <= (radius_distance + contact_tolerance)^2 + (1 - z_var) * max_distance^2)
-            set_name(contact_con, "ContactCon[$(EDGE_ID(edge1)),$node1,$(EDGE_ID(edge2))]")
+            set_name(contact_con, "ContactCon[$(edge1),$node1,$(edge2)]")
 
             # ! collision constraint
             other1 = get_other(edge1, node1)
@@ -318,10 +361,10 @@ function solve(nodes, edges, aabb;
                 for l2 in enumerate_steps(edge2, nodes, edges)
                     # point2 = (1 - l2) * var2 + (l2 * x_vars[edge2, other2])
                     point2 = convex_combination(var2, x_vars[edge2, other2], l2)
-                    difference = point2 - point1
                     # Only neighbors
+                    difference = point2 - point1
                     collision_con = @constraint(model, sum(difference .* difference) >= (radius_distance + buffer_tolerance)^2)
-                    set_name(collision_con, "CollisionCon[$(EDGE_ID(edge1)),$node1,$(EDGE_ID(edge2))]")
+                    set_name(collision_con, "CollisionCon[$(edge1),$node1,$(edge2)]")
 
                     # contact_vars[edge1, edge2, node1] = (z_var, l1_var, point1_var, l2_var, point2_var)
                     contact_vars[edge1, edge2, node1] = z_var
@@ -330,19 +373,32 @@ function solve(nodes, edges, aabb;
         end
     end
 
+    if check_feasible
+        for ((edge, node), x_var) in x_vars
+            xf_con = @constraint(model, x_vars[edge, node] == hint_x_solution[edge, node])
+            set_name(xf_con, "XFeasible[$(edge), $node]")
+        end
+        for ((pair, node), z_var) in z_vars
+            zf_con = @constraint(model, z_vars[pair, node] == hint_z_solution[pair, node])
+            set_name(zf_con, "ZFeasible[$(edge)), $node]")
+        end
+    end
+
     ###################################
 
+    println("="^20)
     status=optimize!(model) # time to optimize!
-    @printf("Objective: %s | # Solutions: %d | Status: %s", objective_value(model), result_count(model), termination_status(model))
+    println("="^20)
+    @printf("Objective: %s | # Solutions: %d | Status: %s\n", objective_value(model), result_count(model), termination_status(model))
 
     if result_count(model) <= 0 || !has_values(model)
         error("The model was not solved correctly.")
         return
     end
 
-    for (edge, neighbors) in z_var_from_edge
-        println("Edge $(EDGE_ID(edge)) : neighbors $(value.(values(neighbors)))")
-    end
+    # for (edge, neighbors) in z_var_from_edge
+    #     println("Edge $(EDGE_ID(edge)) : neighbors $(value.(values(neighbors)))")
+    # end
 
     x_sol = Dict((edge, node) => value.(var)
                 for ((edge, node), var) in x_vars)
@@ -350,8 +406,8 @@ function solve(nodes, edges, aabb;
     z_sol = Dict((edge_pair, node) => value.(var)
                 for ((edge_pair, node), var) in z_vars)
 
-    println(x_sol)
-    println(z_sol)
+    println("X solution: ", x_sol)
+    println("Z solution: ", z_sol)
     return x_sol, z_sol
 end
 
@@ -361,7 +417,7 @@ function main(file_name, args)
     try
         json_data = json_data["overall_structure"] # bar_structure | overall_structure
     catch
-        println(YELLOW_FG, "No overall_structure stored in the data.")
+        println(YELLOW_FG("No overall_structure stored in the data."))
     end
 
     # bar_structure: adjacency, attributes, edge, node
@@ -370,8 +426,9 @@ function main(file_name, args)
     neighbors_from_node = Dict(tryparse(Int,n) => Set(keys(neighbors)) for (n, neighbors) in json_data["adjacency"])
     # println("neighnor from node", neighbors_from_node)
 
-    edges = Dict(Set([tryparse(Int,n1), tryparse(Int,n2)]) => info for (n1, neighbors) in json_data["edge"] for (n2, info) in neighbors)
+    edges = Dict(EDGE_ID([tryparse(Int,n1), tryparse(Int,n2)]) => info for (n1, neighbors) in json_data["edge"] for (n2, info) in neighbors)
     for info in values(edges)
+        # ! enforce uniform bar radius for now
         info["radius"] = 3.17
         info["radius"] *= SCALE
     end
@@ -381,12 +438,11 @@ function main(file_name, args)
 
     points = [info["point"] for info in values(nodes)]
     aabb = PyPb.aabb_from_points(points)
-    # println("aabb: ", aabb)
 
-    scale = 2.
+    bb_scale = 2.
     center = PyPb.get_aabb_center(aabb)
     extent = PyPb.get_aabb_extent(aabb)
-    aabb = PyPb.AABB(lower=center - scale*extent/2, upper=center + scale*extent/2)
+    aabb = PyPb.AABB(lower=center - bb_scale*extent/2, upper=center + bb_scale*extent/2)
     println("aabb: ", aabb)
 
     viewer = args["viewer"]
@@ -397,12 +453,11 @@ function main(file_name, args)
         append!(handles, [PyPb.add_line(nodes[n1]["point"], nodes[n2]["point"], color=PyPb.RED) for (n1, n2) in keys(edges)])
         center_viewer(nodes)
 
-        x_sol, z_sol = solve(nodes, edges, aabb; optimize=args["optimize"])
+        x_sol, z_sol = solve(nodes, edges, aabb; optimize=args["optimize"], num_solutions=args["num_solutions"])
 
-        if viewer
-            visualize_solution(edges, x_sol, z_sol)
-        end
-        println(GREEN_FG, "Solution found!")
+        visualize_solution(nodes, edges, x_sol, z_sol)
+
+        println(GREEN_FG("Solution found!"))
         PyPb.wait_if_gui("Finished.")
     finally
         PyPb.disconnect()
@@ -418,12 +473,16 @@ end
 parser = ArgParseSettings()
 @add_arg_table! parser begin
     "--problem"
-	    help = "json file name"
+	    help = "json file name for the original graph to be solved upon"
         arg_type = String
         default = "truss_one_tet_skeleton.json"
-    # file_name = "cube_skeleton.json"
-    # file_name = "2_tets.json"
-    # file_name = "single_tet_point2triangle.json"
+        # file_name = "cube_skeleton.json"
+        # file_name = "2_tets.json"
+        # file_name = "single_tet_point2triangle.json"
+    "--saved_solution"
+	    help = "json file name for a computed solution, for viz or feasiblity checking"
+        arg_type = String
+        default = "truss_one_tet.json"
     "--viewer"
         help = "Enable pybullet viewer"
         arg_type = Bool
@@ -432,7 +491,12 @@ parser = ArgParseSettings()
         # help = ""
         arg_type = Bool
         default = true
+    "--num_solutions"
+        help = "Limits the number of feasible MIP solutions found. https://www.gurobi.com/documentation/9.0/refman/solutionlimit.html"
+        arg_type = Int
+        default = 1
 end
 
 args = parse_args(parser)
+println(YELLOW_FG("Args: $(args)"))
 main(args["problem"], args)
